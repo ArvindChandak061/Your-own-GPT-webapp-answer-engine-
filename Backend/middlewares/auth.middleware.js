2f801a178dbef052f15f77136c2427820b335e84
ArvindChandak061 <maasaraswati654@gmail.com>
2026-07-31 22:21:54 +0530

    Update auth.middleware.js

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken"
 import tokenBlacklistModel from "../models/blacklist.model.js"
 
-
-
 async function authUser(req, res, next) {
-
-    const token = req.cookies.token
+    const authHeader = req.headers.authorization
+    const token = authHeader && authHeader.startsWith("Bearer ")
+        ? authHeader.split(" ")[1]
+        : null
 
     if (!token) {
         return res.status(401).json({
@@ -16,7 +16,6 @@ async function authUser(req, res, next) {
     const isTokenBlacklisted = await tokenBlacklistModel.findOne({
         token
     })
-
     if (isTokenBlacklisted) {
         return res.status(401).json({
             message: "token is invalid"
@@ -25,19 +24,13 @@ async function authUser(req, res, next) {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
         req.user = decoded
-
         next()
-
     } catch (err) {
-
         return res.status(401).json({
             message: "Invalid token."
         })
     }
-
 }
-
 
 export default { authUser }
