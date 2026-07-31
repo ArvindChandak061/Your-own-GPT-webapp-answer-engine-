61387b1e3445abdc0a5631d0f1b233026d5d835c
ArvindChandak061 <maasaraswati654@gmail.com>
2026-07-31 21:38:55 +0530

    Update Sidebar.jsx

--- Frontend/src/features/chat/components/Sidebar.jsx
+++ Frontend/src/features/chat/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import "../styles/Sidebar.css";
 import { useContext, useEffect } from "react";
 import { MyContext } from "../MyContext.jsx";
 import {v1 as uuidv1} from "uuid";
-import logo from "src/assets/blacklogo.png";
+import logo from "../../../assets/blacklogo.png";
 
 function Sidebar() {
     const {allThreads, setAllThreads, currThreadId, setNewChat, setPrompt, setReply, setCurrThreadId, setPrevChats} = useContext(MyContext);
